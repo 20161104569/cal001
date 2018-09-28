ebcc68996da65aa81f12d0c37abf108277e34077
20161104569 <2174037646@qq.com>
2018-09-28 16:27:11 +0800

    计算器
    利用强制转换实现计算器

--- cal001/ViewController.swift
+++ cal001/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController {
+    @IBOutlet weak var gxta: UITextField!
+    @IBOutlet weak var gxtc: UITextField!
+    @IBOutlet weak var gxtb: UITextField!
+    @IBAction func test1(_ sender: Any) {
+      gxtc.text = "\(Int(gxta.text!)! - Int(gxtb.text!)!)"
+    }
+    @IBOutlet weak var gxtz: UITextField!
+    @IBOutlet weak var gxty: UITextField!
+    @IBOutlet weak var gxtx: UITextField!
+    @IBAction func test(_ sender: Any) {
+        gxtz.text = "\(Int(gxtx.text!)! + Int(gxty.text!)!)"   }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
